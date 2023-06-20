13ed11dea5596c9fdc00977d395d6b1476edfc0d
CRY-TeX <blacklion.x0@gmail.com>
2023-06-20 15:41:53 +0200

    Remove unnecessary training related stuff from repo in order to only provide the colorizer package

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # cv-black-white-images-to-color
+
+## Installation
+
+To install the `colorizer` package run following command:
+
+```bash
+pip install git+https://github.com/CRY-TeX/cv-black-white-images-to-color#egg=colorizer@package
+```
--- start-tf-notbook.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-
-image_name="tf-notebook"
-
-docker build -t $image_name $SCRIPT_DIR
-docker run --rm --gpus all -d -p 8888:8888 -v "$SCRIPT_DIR:/app" --name $image_name $image_name
-docker logs -f $image_name
